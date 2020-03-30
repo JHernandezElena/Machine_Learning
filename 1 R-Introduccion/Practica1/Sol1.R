8361a3cd974329ff5fb183f7bebf1ff1222c37f6
JHernandezElena <jhernandezelena@gmail.com>
2020-03-30 18:41:06 +0200

    Update

--- 1 R-Introduccion/Practica1/Sol1.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-#problema de clasificacion
-sim5 = read.table(file = "SimData5.csv",sep = ";", header = TRUE) 
-View(sim5)
-
-#dibujo
-ggplot(sim5) + geom_point(aes(x=X1, y = X2, color = Y))
-#tablero de ajedrez
